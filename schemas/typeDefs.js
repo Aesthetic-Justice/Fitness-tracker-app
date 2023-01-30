8855b2c547c194b88cf1e0dd0ab7feb648c665c6
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-01-30 18:30:44 -0500

    initial

---
+++ schemas/typeDefs.js
@@ -0,0 +1,11 @@
+const { gpl } = require(`apollo-server-express`);
+
+const typeDefs = gpl`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+    }
+    
+    `
