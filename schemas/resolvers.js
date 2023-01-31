c54cb00ef1f7879b228ebaced574566f4b697b10
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-01-30 19:55:24 -0500

    added resolver template

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,7 +1,14 @@
-const {} = require(`../models`);
+const { User, Workout, Set } = require(`../models`);
+const { AuthenticationError } = require(`apollo-server-express`);
+/* const { signToken } = require(`../utils`); */
 
 const resolvers = {
     Query: {
 
+    },
+    Mutation: {
+
     }
 };
+
+module.exports = resolvers;
