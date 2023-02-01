f0e757f7c256ea49c31c6940bc05ead3b2897b25
Troy Reid-Knight <troy@Troys-MacBook-Pro.local>
2023-02-01 17:43:56 -0500

    Adding resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,26 @@
 const { User, Workout, Set } = require(`../models`);
 const { AuthenticationError } = require(`apollo-server-express`);
+const { ProfilingLevel } = require("mongodb");
 /* const { signToken } = require(`../utils`); */
 
 const resolvers = {
   Query: {
-    //
+    users: async () => {
+      return User.find();
+    },
+
+    user: async (parent,{userId}) => {
+      return User.findone({_id:userId})
+    }
+    
   },
   Mutation: {
-    // TODO: addUser
+    Signup: async(parent, {username,email,password}) => {
+      return User.create({username,email,password});
+    },
+    Login: async(parent,{username,password}) =>{
+      
+    }
   },
 };
 
