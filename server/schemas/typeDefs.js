9a1784e74cefc1e52eda839ddbab4a2f180f7725
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-02-03 18:48:47 -0500

    cleaned up new mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,6 +39,8 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     signup(username: String!, email: String!, password: String!): Auth
+    addWorkoutToUser(workoutId: ID!): User
+    removeWorkoutFromUser(workoutId: ID!): User
   }
 `;
 
