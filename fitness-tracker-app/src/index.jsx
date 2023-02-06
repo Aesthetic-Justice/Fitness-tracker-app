d193dc4822422e02a4db6ae1784b59f173419fb2
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-02-06 14:35:04 -0500

    swapped to createRoot

--- fitness-tracker-app/src/index.jsx
+++ fitness-tracker-app/src/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <HashRouter>
     <App />
-  </HashRouter>,
-  document.getElementById("root")
+  </HashRouter>
 );
