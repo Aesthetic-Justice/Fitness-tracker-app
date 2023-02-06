59231ee7e0f6ee893e290e16b82539a41e7b4a11
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-02-06 16:19:49 -0500

    Clear workout page for useMutation

--- fitness-tracker-app/src/components/workout.jsx
+++ fitness-tracker-app/src/components/workout.jsx
@@ -3,6 +3,7 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
+
 import { Container, ListGroup } from 'react-bootstrap';
 import WorkoutType from './WorkoutType';
 
@@ -12,6 +13,11 @@ function WorkoutForm(sets) {
   for (const iterator of sets.sets) {
     num++;
     WorkoutArray.push(<WorkoutType data={iterator} key={num} />)
+  }
+
+  const PushWorkout = (e) =>{
+    e.preventDefault();
+    console.log('hello world');
   }
 
   return (
@@ -28,12 +34,6 @@ function WorkoutForm(sets) {
           </Col>
 
           <Col >
-            <FloatingLabel controlId="floatingSelect" label="Enter the name of your workout">
-              <Form.Control type="textarea" placeholder="Enter the name of your workout" />
-            </FloatingLabel>
-          </Col>
-
-          <Col >
             <FloatingLabel controlId="floatingSelect" label="Date">
               <Form.Control type="date" placeholder="Date" />
             </FloatingLabel>
@@ -41,72 +41,7 @@ function WorkoutForm(sets) {
         </Row>
 
 
-        <Row className="mb-3">
-          <Col >
-            <FloatingLabel controlId="floatingSelect" label="Sets (1-10)">
-              <Form.Select aria-label="Floating label select example">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option>N/A</option>
-              </Form.Select>
-            </FloatingLabel>
-          </Col>
-
-          <Col >
-            <FloatingLabel controlId="floatingSelect" label="Reps (1-10)">
-              <Form.Select aria-label="Floating label select example">
-
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option>N/A</option>
-              </Form.Select>
-            </FloatingLabel>
-          </Col>
-
-          <Col >
-            <FloatingLabel controlId="floatingSelect" label="Duration">
-              <Form.Control type="textarea" placeholder="Enter the name of your workout" />
-            </FloatingLabel>
-          </Col>
-
-        </Row>
-
-
-        {/* 
-      <Row className="mb-3">
-      
-      <Col >
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Intensity</Form.Label>
-        <Form.Control placeholder="Please enter your intensity" />
-      </Form.Group>
-      </Col>
-      <Col >
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>Target</Form.Label>
-          <Form.Control />
-        </Form.Group>
-        </Col>
-      </Row> */}
-
-
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={PushWorkout}>
           Submit
         </Button>
       </Form>
